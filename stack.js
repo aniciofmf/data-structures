00ddc6316f4ed05c0124e56c7e4c76ac0a958dae
fmfontana <105196681+fmfontana@users.noreply.github.com>
2022-06-19 20:17:37 -0300

    stack

---
+++ stack.js
@@ -0,0 +1,62 @@
+/* Link based impl */
+
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Stack {
+	constructor() {
+		this.top = null;
+		this.bottom = null;
+		this.length = 0;
+	}
+
+	peek() {
+		return this.length > 0 ? this.top.value : null;
+	}
+
+	push(value) {
+		var node = new Node(value);
+
+		if (this.length == 1) {
+			this.bottom = this.top;
+		}
+
+		node.next = this.top;
+		this.top = node;
+
+		this.length++;
+	}
+
+	pop() {
+		var value = null;
+
+		if (this.length == 1) {
+			value = this.top.value;
+			this.top = null;
+			this.bottom = null;
+		} else {
+			value = this.top.value;
+			this.top = this.top.next;
+		}
+
+		return value;
+	}
+
+	isEmpty() {
+		return this.length == 0 ? true : false;
+	}
+}
+
+const myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+myStack.push(4);
+
+var value = myStack.pop();
+console.log(value);
