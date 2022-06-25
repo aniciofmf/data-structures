85c292f24651d61ca07ea03ee92747d0693e82d4
fmfontana <105196681+fmfontana@users.noreply.github.com>
2022-06-25 12:08:33 -0300

    Create selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,18 @@
+const numbers = [9, 40, 6, 2, 1, 5, 69, 87, 283, 600, 544, 704, 8, 4, 0];
+
+function selectionSort(array) {
+	for (let i = 0; i < array.length; i++) {
+		let min = i;
+		let temp = array[i];
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] < array[min]) {
+				min = j;
+			}
+		}
+		array[i] = array[min];
+		array[min] = temp;
+	}
+}
+
+selectionSort(numbers);
+console.log(numbers);
