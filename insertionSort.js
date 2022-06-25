f0e9d60be9f62f22245d004e05eed53faef6cdb4
fmfontana <105196681+fmfontana@users.noreply.github.com>
2022-06-25 18:41:03 -0300

    Create insertionSort.js

---
+++ insertionSort.js
@@ -0,0 +1,20 @@
+const numbers = [9, 40, 10, 2, 1, 5, 69, 87, 283, 0, 600, 544, 704, 8, 4];
+
+function insertionSort(array) {
+	for (let i = 1; i < array.length; i++) {
+		let pos = i - 1;
+
+		if (array[i] < array[pos]) {
+			let elem = array[i];
+			array[i] = null;
+			for (var j = pos; elem < array[j] && j >= 0; j--) {
+				array[j + 1] = array[j];
+			}
+
+			array[j + 1] = elem;
+		}
+	}
+}
+
+insertionSort(numbers);
+console.log(numbers);
